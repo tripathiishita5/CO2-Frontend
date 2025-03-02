26bc97bfddc0f440a550835b673ae372ee0ec1ab
Harsh-s0ni <hsoni6954@gmail.com>
2025-03-02 17:49:29 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RoleProvider } from './Contexts/RoleContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RoleProvider>
     <App />
-    </RoleProvider>
   </StrictMode>,
 )
